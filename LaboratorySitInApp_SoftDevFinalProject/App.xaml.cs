109b47785d6670b98f55c24d06204c548e4a391f
Xi Milian Excelsis <Xi Milian Excelsis@DESKTOP-R5DB028>
2026-05-05 00:49:47 +0800

    Add project files.

---
+++ LaboratorySitInApp_SoftDevFinalProject/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LaboratorySitInApp_SoftDevFinalProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
